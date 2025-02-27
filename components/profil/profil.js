723431ad2dfb400dd498e49ed46f6265761ab0b4
ragnarleportugais <ribeirotiti93ris@gmail.com>
2025-02-27 23:03:49 +0100

    creation profil

---
+++ components/profil/profil.js
@@ -0,0 +1,1 @@
+import s from "./profil.style";
---
+++ components/profil/profil.style.js
@@ -0,0 +1,6 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window"); // Récupérer la hauteur de l'écran
+
+export const styles = StyleSheet.create({
+});
